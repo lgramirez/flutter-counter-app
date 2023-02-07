37bf9e108a12a7a9179ea17f93d326ebe46c8d0a
lgramirez <gramirez.developer@gmail.com>
2023-02-07 14:25:09 -0400

    Widget Image and FadeInImage

---
+++ lib/widgets/custom_card_type_2.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CustomCardType2 extends StatelessWidget {
+  const CustomCardType2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 10,
+      child: Column(
+        children: const [
+          FadeInImage(
+            image: NetworkImage(
+                'https://images.pexels.com/photos/1619317/pexels-photo-1619317.jpeg?cs=srgb&dl=pexels-james-wheeler-1619317.jpg&fm=jpg'),
+            placeholder: AssetImage('assets/jar-loading.gif'),
+          )
+        ],
+      ),
+    );
+  }
+}
