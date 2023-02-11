418b9f49eca5fd73b49cf1a06c603581325080a6
lgramirez <gramirez.developer@gmail.com>
2023-02-11 11:18:50 -0400

    Display an alert

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -3,6 +3,39 @@ import 'package:flutter/material.dart';
 class AlertScreen extends StatelessWidget {
   const AlertScreen({Key? key}) : super(key: key);
 
+  void displayDialog(BuildContext context) {
+    showDialog(
+        // poniendo esta propiedad en true permite que podamos apretar
+        // en el fondo del elemento y cerrar esta alerta
+        barrierDismissible: false,
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            elevation: 5,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadiusDirectional.circular(10)),
+            title: const Text('titulo'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                Text('este es el contenido de la alerta'),
+                SizedBox(
+                  height: 10,
+                ),
+                FlutterLogo(
+                  size: 100,
+                )
+              ],
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('Cancelar'))
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +53,7 @@ class AlertScreen extends StatelessWidget {
               style: TextStyle(fontSize: 16),
             ),
           ),
-          onPressed: () {},
+          onPressed: () => displayDialog(context),
         ),
       ),
       floatingActionButton: FloatingActionButton(
