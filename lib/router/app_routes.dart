e3f7e6fa6532f177074dfdb1828c8ee9f628ab54
lgramirez <gramirez.developer@gmail.com>
2023-02-14 09:34:00 -0400

    Widget Container

--- lib/router/app_routes.dart
+++ lib/router/app_routes.dart
@@ -37,6 +37,11 @@ class AppRoutes {
         icon: Icons.supervised_user_circle_outlined,
         name: 'Circle Avatar',
         screen: const AvatarScreen()),
+    MenuOption(
+        route: 'animated',
+        icon: Icons.play_circle_outline_rounded,
+        name: 'Animated Container',
+        screen: const AnimatedScreen()),
   ];
 
   static Map<String, Widget Function(BuildContext)> getAppRoutes() {
