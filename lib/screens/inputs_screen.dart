66c1f08eed8bc1e44d7ca63e7996cdc8ec1b2052
lgramirez <gramirez.developer@gmail.com>
2023-02-19 12:43:25 -0400

    Add validator and styles

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -15,10 +15,27 @@ class InputsScreen extends StatelessWidget {
           child: Column(
             children: [
               TextFormField(
-                autofocus: true,
+                autofocus: false,
                 initialValue: 'Gonzalo',
                 textCapitalization: TextCapitalization.words,
                 onChanged: (value) => {print('value: $value')},
+                validator: (value) {
+                  if (value == null) return 'Este campo es requerido';
+                  return value.length < 3 ? 'Minimo de 3 letras' : null;
+                },
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: const InputDecoration(
+                    hintText: 'Nombre del usuario',
+                    labelText: 'Nombre',
+                    helperText: 'Solo letras',
+                    // prefixIcon: Icon(Icons.verified_user_outlined),
+                    suffixIcon: Icon(Icons.group_outlined),
+                    icon: Icon(Icons.assignment_ind_outlined),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(10),
+                      topRight: Radius.circular(10),
+                    ))),
               )
             ],
           ),
