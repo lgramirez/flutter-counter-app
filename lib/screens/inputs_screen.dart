3efa0bed2e0bf9fb21577b23c36b8efe02c124d1
lgramirez <gramirez.developer@gmail.com>
2023-02-22 17:16:35 -0400

    DropdownButtonFormField widget

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -64,6 +64,22 @@ class InputsScreen extends StatelessWidget {
                 const SizedBox(
                   height: 30,
                 ),
+                DropdownButtonFormField<String>(
+                    value: 'Admin',
+                    items: const [
+                      DropdownMenuItem(value: 'Admin', child: Text('Admin')),
+                      DropdownMenuItem(
+                          value: 'Superuser', child: Text('Superuser')),
+                      DropdownMenuItem(
+                          value: 'Developer', child: Text('Developer')),
+                      DropdownMenuItem(
+                          value: 'Jr. Developer', child: Text('Jr. Developer')),
+                      DropdownMenuItem(
+                          value: 'Mid. Developer',
+                          child: Text('Mid. Developer'))
+                    ],
+                    onChanged: (value) =>
+                        formValues['role'] = value ?? 'Admin'),
                 ElevatedButton(
                     onPressed: () {
                       FocusScope.of(context).requestFocus(FocusNode());
